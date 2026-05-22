4ec1bc48bfdaa589318ad54ef95110d9b0c2eb2a
Sanyul-islam <imsanyulislam@gmail.com>
2026-05-23 05:18:09 +0600

    live domain updated

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,8 @@
 export const TilesData = async() => {
 
-    const Promise = await fetch("http://localhost:3000/data.json");
+    const Promise = await fetch(
+      "https://tiles-gallery-3vwqzad1d-imsanyulislam-1529s-projects.vercel.app/data.json",
+    );
     const data = await Promise.json();
     return data;
 }
