8184c679213de8dd619933fd9cd476116fdc0835
Sanyul-islam <imsanyulislam@gmail.com>
2026-05-23 05:38:04 +0600

    link updated

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,7 +1,7 @@
 export const TilesData = async() => {
 
     const Promise = await fetch(
-      "https://tiles-gallery-lilac-seven.vercel.app/data.json",
+      `${process.env.NEXT_PUBLIC_BASE_URL}/data.json`
     );
     const data = await Promise.json();
     return data;
